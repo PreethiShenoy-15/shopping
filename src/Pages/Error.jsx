339d9b3b46096cdeef2f4a99da687b5c9446f855
PreethiShenoy-15! <shenoyradha4@gmail.com>
2025-03-01 12:00:17 +0530

    'i-did-changes-in-alt-text'

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Error() {
-  return <div>Error</div>;
+  return <div>
+    Error 404:Page Not Found
+    <img src="https://sp-ao.shortpixel.ai/client/to_auto,q_glossy,ret_img,w_1600,h_832/https://hummingweb.com.au/wp-content/uploads/2023/03/404-page-not-found.jpg" alt="hello git new branch 01-03-2025-s"/>
+    </div>;
 }
